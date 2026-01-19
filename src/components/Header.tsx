5a946eab335b7c752df82cc89b84dac2b9fa4c50
ImBiswarup <ghoshbiswarup414@gmail.com>
2026-01-19 11:42:06 +0530

    final update 2.0

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link';
 import React from 'react';
 import { usePathname } from 'next/navigation';
-import AuthModal from './AuthModal';
 
 const Header: React.FC = () => {
   const pathname = usePathname();
